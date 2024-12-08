028583ac74fc1d790a9ae155765cc03d64ddc4f6
Hazem Sherif <hazem.sh2002@hotmail.com>
2024-12-08 15:41:47 +0200

    Rogue Finished

---
+++ Assets/Script/Arrow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    
+    private void Start(){
+        Destroy(gameObject,3);
+        
+    }   
+    private void OnTriggerEnter(Collider other){
+        Destroy(transform.GetComponent<Rigidbody>());
+    }
+}
